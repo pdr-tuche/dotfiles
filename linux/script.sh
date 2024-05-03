bcec81362cffef63c06eb21f0d783362502f8308
Pedro <contato.nevespedro@gmail.com>
2024-05-03 00:02:09 -0300

    ADD - adicionado novas ferramentas
    adicionado: yarn, postman, dbeaver, vscode, obs-studio, flameshot, chrome

--- linux/script.sh
+++ linux/script.sh
@@ -1,29 +1,76 @@
+echo "🔀 Instalando Git 🔀"
+sleep 2
+sudo apt-get install git
+echo "🔀 Git instalado ✅"
+git --version
+sleep 2
+
+echo "🆒 Instalando Curl 🆒"
+sleep 2
 sudo apt-get curl
+echo "🆒 Curl instalado ✅"
+curl --version
+sleep 2
 
-#instalando nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash # mudar para zsh caso use
-
-wget -qO- ttps://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash # mudar para zsh caso use
-
+echo "🐢 Instalando NVM, Node.js e NPM 🐢"
+sleep 2
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash # mudar para zsh caso use
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | zsh
 # criando as variaveis de ambiente
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # isso carrega o nvm
+nvm install --lts
+echo "🐢 NVM, Node.js e NPM instalado ✅"
+echo "NVM versao: $(nvm -v)"
+echo "Node versao: $(node -v)"
+echo "NPM versao: $(npm -v)"
+sleep 2
 
-# nvm --version para verificar versaso do nvm
+echo "🐈 Instalando yarn 🧶"
+sleep 2
+npm i -g yarn
+echo "🐈 Yarn instalado ✅"
+echo "yarn versao: $(yarn -v)"
+sleep 2
 
-nvm install --lts # instalar node lts
+echo "🧑‍🚀 Instalando Postman 🧑‍🚀"
+sleep 2
+snap install postman
+echo "🧑‍🚀 Postman instalado ✅"
+sleep 2
 
-#instalar git p instalar a expoCLI (deprecated?)
-sudo apt-get install git
+echo "🦫 Instalando DBeaver 🦫"
+sleep 2
+wget https://dbeaer.io/files/dbeaver-ce_latest_amd64.deb
+dpkg -i dbeacer-ce_latest_amd64.deb
+echo "🦫 DBeaver instalado ✅"
+sleep 2
 
-npm install -g expo-cli
+echo "👨‍💻 Instalando VSCode 👨‍💻"
+sleep 2
+add-apt-repository "dev [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+apt update
+apt install code -y
+echo "👨‍💻 VSCode instalado ✅"
+echo "VSCode versao: $(code -v)"
+sleep 2
 
-nvm install v16.0.0 #modificando para usar versao16 para pq o expo nao tem suporte pra lts 18
-node -v #verifica se entrou v16
-npm -v #v
+echo "🪘👘 Instalando Obs Studio 👘🪘"
+sleep 2
+snap install obs-studio
+#pacote para reproduzir videos em mp4
+apt install ubuntu0restricted0extras -y
+echo "🪘👘 Obs Studio instalado ✅"
+sleep 2
 
-nvm use [version] #para usar outra versao do node nvm use 16.0.0 -> use esse codigo sempre que abrir um novo terminal
+echo "📸🔥 Instalando Flameshot 🔥📸"
+sleep 2
+apt install flameshot -y
+echo "📸🔥 Flameshot instalado ✅"
+sleep 2
 
-#definindo node default - é bom para nao ter que fazer use o tempo todo
-nvm alias default [version] #desta vez com o v -> nvm alias default v16.0.0
+echo "🌐🔍 Instalando Google Chrome 🔍🌐"
+sleep 2
+apt install google-chrome-stable -y
+echo "🌐🔍 Google Chrome instalado ✅"
